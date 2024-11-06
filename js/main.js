c5c6e39d601ea98d020bb9a23407c46da22b02a5
Arthur-Diasz <cdp1821@gmail.com>
2024-11-06 17:01:42 -0300

    Seção serviços

--- js/main.js
+++ js/main.js
@@ -1,33 +1,15 @@
-window.addEventListener("scroll", function () {
-  const navbar = document.querySelector(".navbar");
-  if (window.scrollY > 50) {
-    // Altere o valor para ajustar quando o fundo deve aparecer
-    navbar.classList.add("scrolled");
-  } else {
-    navbar.classList.remove("scrolled");
-  }
-});
+// Função para detectar quando os elementos entram na tela
+function scrollAnimation() {
+  const elements = document.querySelectorAll("[data-scroll]");
+  elements.forEach((element) => {
+    const position = element.getBoundingClientRect().top;
+    const windowHeight = window.innerHeight;
+    if (position - windowHeight <= -50) {
+      element.classList.add("visible");
+    }
+  });
+}
 
-
-document.addEventListener("DOMContentLoaded", function () {
-  const animateElements = document.querySelectorAll(".animate");
-
-  function animateOnScroll() {
-    animateElements.forEach((element) => {
-      const elementTop = element.getBoundingClientRect().top;
-      const windowHeight = window.innerHeight;
-
-      // Checa se o elemento está visível (ajuste o valor de 150 conforme necessário)
-      if (elementTop < windowHeight - 150) {
-        element.classList.add("show");
-      }
-    });
-  }
-
-  window.addEventListener("scroll", animateOnScroll);
-  animateOnScroll(); // Chama a função para o caso do elemento já estar na tela
-});
-
-
-
-
+// Ativar animação ao rolar a página
+window.addEventListener("scroll", scrollAnimation);
+window.addEventListener("load", scrollAnimation);
